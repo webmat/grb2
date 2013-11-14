610fac19ac85202e3403c97ea1491de241c747d3
Mathieu Martin <webmat@gmail.com>
2013-11-14 13:43:37 -0500

    Skeleton of the app, with one dummy method

--- lib/grb.rb
+++ lib/grb.rb
@@ -1,5 +1,11 @@
 require "grb/version"
+require 'thor'
 
 module Grb
-  # Your code goes here...
+  class CLI < Thor
+    desc "Delete a branch", "Delete a branch locally and remotely"
+    def delete(branch, remote = 'origin')
+      puts "deleting #{branch} #{remote}"
+    end
+  end
 end
